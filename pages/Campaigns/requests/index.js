6198d3c5a91ffcd7fa58381fc23cf40b65521bcc
Colin Schlecht <schlechtcolin@yahoo.com>
2021-05-14 02:30:24 -0700

    adds request creation page/route

--- pages/Campaigns/requests/index.js
+++ pages/Campaigns/requests/index.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
+import Layout from "../../../components/Layout";
+import { Button } from "semantic-ui-react";
+import { Link } from "../../../routes";
 
 class RequestsIndex extends Component {
+  static async getInitialProps(props) {
+    const { address } = props.query;
+    return { address };
+  }
   render() {
     return (
-      <div>
+      <Layout>
         <h3>Requests</h3>
-      </div>
+        <Link route={`/campaigns/${this.props.address}/requests/new`}>
+          <a>
+            <Button primary>Add Request</Button>
+          </a>
+        </Link>
+      </Layout>
     );
   }
 }
---
+++ pages/Campaigns/requests/new.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class RequestsNew extends Component {
+  render() {
+    return <h3>Create a New Request</h3>;
+  }
+}
+export default RequestsNew;
