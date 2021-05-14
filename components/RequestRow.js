4c9bcabd3f186256e1c64e4fedd084dd2b1cd09d
Colin Schlecht <schlechtcolin@yahoo.com>
2021-05-14 16:02:24 -0700

    adds table to requests

---
+++ components/RequestRow.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class RequestRow extends Component {
+  render() {
+    return <div>Request!!!</div>;
+  }
+}
+
+export default RequestRow;
--- pages/Campaigns/requests/index.js
+++ pages/Campaigns/requests/index.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
 import Layout from "../../../components/Layout";
-import { Button } from "semantic-ui-react";
+import { Button, Table } from "semantic-ui-react";
 import { Link } from "../../../routes";
-
+import Campaign from "../../../ethereum/campaign";
 class RequestsIndex extends Component {
   static async getInitialProps(props) {
     const { address } = props.query;
-    return { address };
+    const campaign = Campaign(address);
+    const requestCount = await campaign.methods.getRequestsCount().call();
+
+    const requests = await Promise.all(
+      Array(parseInt(requestCount))
+        .fill()
+        .map((element, index) => {
+          return campaign.methods.requests(index).call();
+        })
+    );
+    return { address, requests, requestCount };
   }
   render() {
+    const { Header, Row, HeaderCell, Body } = Table;
     return (
       <Layout>
         <h3>Requests</h3>
@@ -17,6 +28,19 @@ class RequestsIndex extends Component {
             <Button primary>Add Request</Button>
           </a>
         </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+        </Table>
       </Layout>
     );
   }
